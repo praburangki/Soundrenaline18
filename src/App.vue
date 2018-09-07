3e56d843c02ed41d7d2cff5ec64716bc7a6ac01e
praburangki <praburangki@gmail.com>
2018-09-07 23:29:31 +0800

    make into just one single page

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,87 @@
 <template>
   <v-app>
-    <v-toolbar app extended color="primary" dark class="mainNav">
+    <v-toolbar app color="primary" dark tabs>
       <v-toolbar-title class="appTitle">Soundrenaline 2018</v-toolbar-title>
-      <div slot="extension" class="navLinks">
-        <v-btn round color="white" light slot="extension" to="/">SCHEDULE</v-btn>
-        <v-btn round color="white" light slot="extension" to="/lineup" outline>LINE UP</v-btn>
-      </div>
+
+      <v-tabs
+        slot="extension"
+        v-model="tabModel"
+        grow
+        color="primary"
+        slider-color="yellow"
+      >
+        <v-tabs-slider color="black"></v-tabs-slider>
+        <v-tab href="#table">
+          <v-icon>dashboard</v-icon>
+        </v-tab>
+        <v-tab href="#list">
+          <v-icon>view_list</v-icon>
+        </v-tab>
+        <v-tab href="#favorite">
+          <v-icon>favorite</v-icon>
+        </v-tab>
+        <v-tab href="#pictures">
+          <v-icon>photo</v-icon>
+        </v-tab>
+      </v-tabs>
     </v-toolbar>
-    <v-content>
-      <router-view/>
+
+    <v-content class="mb-5">
+      <v-tabs-items v-model="tabModel">
+        <v-tab-item id="table" lazy>
+          <BtnDaySwitcher />
+          <TableView />
+        </v-tab-item>
+
+        <v-tab-item id="list" lazy>
+          <BtnDaySwitcher />
+          <ListView />
+        </v-tab-item>
+
+        <v-tab-item id="favorite" lazy>
+          <v-card flat>
+            <v-card-text>favorite</v-card-text>
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
     </v-content>
-    <v-footer app>
+    <v-footer class="justify-center py-2" fixed height="60">
+      <div>
+        <v-btn color="primary">
+          info
+          <v-icon right dark>info</v-icon>
+        </v-btn>
+      </div>
     </v-footer>
   </v-app>
 </template>
 
 <script>
+import TableView from '@/components/TableView';
+import ListView from '@/components/ListView';
+import BtnDaySwitcher from '@/components/BtnDaySwitcher';
+
 export default {
-  name: 'App',
+  components: {
+    TableView,
+    ListView,
+    BtnDaySwitcher
+  },
+  created() {
+    this.$store.dispatch('fetchArtists');
+  },
   data() {
     return {
-      title: 'Vuetify.js'
+      tabModel: 'table'
     };
   }
 };
 </script>
 
 <style lang="stylus" scoped>
-.mainNav {
-  padding-bottom: 10px;
-}
-
 .appTitle {
   font-family: 'Product Sans';
   text-transform: uppercase;
-  margin: 0 auto;
-}
-
-.navLinks {
   margin: 0 auto;
 }
 </style>
